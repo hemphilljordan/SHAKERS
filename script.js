8ef51c0247c42b7caa73b8bc9a2db3252e0b357c
Jordan Hemphill <hemphillmusic@gmail.com>
2025-05-20 14:29:48 -0500

    test mobile

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
     }
   });
 
+
+
+
+  function toggleMobileMenu(menu){
+  menu.classList.toggle('open');
+}
+
+function clearPlaceholder(input) {
+  input.placeholder = '';
+}
+
+function toHeadshot(){
+  window.location.href = "headshots.html"
+}
